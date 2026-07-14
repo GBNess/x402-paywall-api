f69026eb559f071997b2af49f263c93296f61971
GBNess <nessf1984@gmail.com>
2026-07-14 15:05:20 -0700

    Delete api/index.ts

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  res.status(200).json({
-    success: true,
-    message: "You have successfully accessed the premium paywall data!"
-  });
-};
